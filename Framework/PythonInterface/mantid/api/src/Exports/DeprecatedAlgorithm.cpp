f362f930f95a7f14b262daafc4e972435a0acb46
Gagik Vardanyan <vardanyan@ill.fr>
2016-12-22 16:05:19 +0100

    Re #18326 option 1 (not working)

--- Framework/PythonInterface/mantid/api/src/Exports/DeprecatedAlgorithm.cpp
+++ Framework/PythonInterface/mantid/api/src/Exports/DeprecatedAlgorithm.cpp
@@ -1,5 +1,4 @@
 #include "MantidAPI/DeprecatedAlgorithm.h"
-#include "MantidPythonInterface/kernel/GetPointer.h"
 #include <boost/python/class.hpp>
 
 using namespace Mantid::API;
@@ -14,17 +13,6 @@ void export_DeprecatedAlgorithm() {
            (arg("self"), arg("alg"), arg("version")),
            "Set the name of the algorithm to use instead")
 
-      .def("deprecationMsg", &DeprecatedAlgorithm::deprecationMsg,
-           (arg("self"), arg("ialg")), "Get the deprecation message")
-
       .def("deprecatedDate", &DeprecatedAlgorithm::deprecatedDate,
            (arg("self"), arg("date")), "Set the deprecation date");
 }
-
-//#include "MantidPythonInterface/api/PythonAlgorithm/DeprecatedAdapter.h"
-//#include <boost/python/register_ptr_to_python.hpp>
-//using Mantid::PythonInterface::DeprecatedAdapter;
-//GET_POINTER_SPECIALIZATION(DeprecatedAlgorithm)
-//register_ptr_to_python<boost::shared_ptr<DeprecatedAlgorithm>>();
-//.add_property("deprecatedDate", &DeprecatedAlgorithm::deprecatedDate)
-//.add_property("useAlgorithm", &DeprecatedAlgorithm::useAlgorithm)
